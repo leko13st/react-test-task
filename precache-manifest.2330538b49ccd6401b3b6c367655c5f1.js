6665ed11097766e94aa3bb1c9848bcadccb6895a
leko13st <lekomstas13@mail.ru>
2020-08-19 23:46:06 +0400

    Updates

---
+++ precache-manifest.2330538b49ccd6401b3b6c367655c5f1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fe33f358b2683f6e966b819947a6d1fc",
+    "url": "/react-test-task/index.html"
+  },
+  {
+    "revision": "080fe2311276e5910365",
+    "url": "/react-test-task/static/css/main.ae768d75.chunk.css"
+  },
+  {
+    "revision": "222cd8073435766c0819",
+    "url": "/react-test-task/static/js/2.44cc70c4.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/react-test-task/static/js/2.44cc70c4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "080fe2311276e5910365",
+    "url": "/react-test-task/static/js/main.237bb75d.chunk.js"
+  },
+  {
+    "revision": "1adb4df5858ede265f8e",
+    "url": "/react-test-task/static/js/runtime-main.73a6fb8a.js"
+  },
+  {
+    "revision": "09fb6445238893a384ae4e25f094ea76",
+    "url": "/react-test-task/static/media/bg.09fb6445.jpg"
+  }
+]);
